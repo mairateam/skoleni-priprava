a8247d2f942090993eb20919b9a222638bf7b4fe
Michal Filipek <michal.filipek@databy.io>
2026-04-02 08:15:35 +0200

    ficury

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/b/bc/Socrate_du_Louvre.jpg" width={250}></img>
+      <h1>Sokrates kazi mladez, protoze:</h1>
+      <ul>
+        <li>Hani stare bohy</li>
+        <li>Zavadi nova bozstva</li>
+      </ul>
+    </main>
+  )
+}
+
+export default App
